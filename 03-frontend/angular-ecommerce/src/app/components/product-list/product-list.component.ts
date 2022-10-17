182355dad67be9c89a62094f0eb7cb613b9d6810
ciaranmckenna <hello@ciaranmckenna.com>
2022-10-17 09:27:50 +0100

    Display category name in product list grid

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
   currentCategoryId: number = 1;
+  currentCategoryName: string = ""; 
 
   // dependency injected 
   constructor(private productService: ProductService, 
@@ -28,11 +29,16 @@ export class ProductListComponent implements OnInit {
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
     if(hasCategoryId){
+      // get the "id" param string. convert string to a number using the "+" symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+     
+      // get the "name" param string
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name')!;
     }
     else{
       // no category id available ... default to category id 1
-      this.currentCategoryId =1;
+      this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
     }
 
     // now get the products for the given category id
