57314ce301e80dec5f5f081079661645bbaf1085
ciaranmckenna <hello@ciaranmckenna.com>
2023-05-11 16:59:53 +0100

    Adding pagination

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -12,8 +12,14 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
   currentCategoryId: number = 1;
+  previousCategoryId: number = 1;
   currentCategoryName: string = "";
   searchMode: boolean = false;
+
+  // new properties for pagination
+  thePageNumber: number = 1;
+  thePageSize: number = 10;
+  theTotalElements: number = 0;
 
   // dependency injected 
   constructor(private productService: ProductService,
@@ -66,13 +72,34 @@ export class ProductListComponent implements OnInit {
       this.currentCategoryName = 'Books';
     }
 
-    // now get the products for the given category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      data => {
-        this.products = data;
+    // Check if we havea different category than previous
+    // Note: Angular will reuse a component if it is currently being viewed
 
-      }
-    )
+    // if we havea different category id than previous
+    // then set thePageNumber back to 1
+
+    if(this.previousCategoryId != this.currentCategoryId){
+      this.thePageNumber = 1;
+    }
+
+    this.previousCategoryId = this.currentCategoryId;
+
+    console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
+
+    // now get the products for the given category id
+    // need to map page size accordingly with Spring Data Rest (pages are 0 based), Angular Pagination component pages are 1 based 
+    this.productService.getProductListPaginate(this.thePageNumber -1, 
+                                              this.thePageSize, 
+                                              this.currentCategoryId)
+                                              .subscribe(
+                                                data => {
+                                                  this.products = data._embedded.products;
+                                                  this.thePageNumber = data.page.number +1;
+                                                  this.thePageSize = data.page.size;
+                                                  this.theTotalElements = data.page.totalElements;
+                                                }
+                                              );
+
 
   }
 
