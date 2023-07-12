05bc7c7cdb05cddb471a6dd7508a0324b6b61013
ciaranmckenna <hello@ciaranmckenna.com>
2023-07-12 16:52:50 +0100

    adding the decrement and remove functionality

--- 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -47,6 +47,31 @@ export class CartService {
     this.computeCartTotals();
 
   }
+
+  decrementQuantity(theCartItem: CartItem){
+
+    theCartItem.quantity --;
+
+    if(theCartItem.quantity == 0){
+      this.remove(theCartItem);
+    }else{
+      this.computeCartTotals();
+    }
+    
+  }
+
+  remove(theCartItem: CartItem) {
+
+    const itemIndex = this.cartItems.findIndex(tempCartItem => tempCartItem.id === tempCartItem.id);
+
+    if(itemIndex > -1){
+      this.cartItems.splice(itemIndex, 1)
+
+      this.computeCartTotals();
+    }
+
+  }
+
   computeCartTotals() {
 
     let totalPriceValue: number = 0;
